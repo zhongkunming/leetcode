3c518c2336a71f8599510de4063f7c33e859e007
unminzzz <unminzzz@163.com>
2022-02-23 09:36:36 +0800

    2.两数相加

--- 2两数相加/main.go
+++ 2两数相加/main.go
@@ -7,27 +7,11 @@ type ListNode struct {
 	Next *ListNode
 }
 
-//[2,4,3]
-//[5,6,4]
-func main() {
-	l13 := &ListNode{Val: 3}
-	l12 := &ListNode{Val: 6, Next: l13}
-	l11 := &ListNode{Val: 2, Next: l12}
-
-	l23 := &ListNode{Val: 3}
-	l22 := &ListNode{Val: 4, Next: l23}
-	l21 := &ListNode{Val: 2, Next: l22}
-	l20 := &ListNode{Val: 5, Next: l21}
-	result := addTwoNumbers(l11, l20)
-	for result != nil {
-		fmt.Println(result.Val)
-		result = result.Next
-	}
-}
-
 /**
 两个链表都是逆序存储的，可以通过直接遍历将两个相同位置的值相加
 
+时间复杂度 O(max(l1, l2))
+空间复杂度 O(1)
 */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	// 声明头指针和尾指针
@@ -57,4 +41,20 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		tail.Next = &ListNode{Val: carry}
 	}
 	return head
+}
+
+func main() {
+	l13 := &ListNode{Val: 3}
+	l12 := &ListNode{Val: 6, Next: l13}
+	l11 := &ListNode{Val: 2, Next: l12}
+
+	l23 := &ListNode{Val: 3}
+	l22 := &ListNode{Val: 4, Next: l23}
+	l21 := &ListNode{Val: 2, Next: l22}
+	l20 := &ListNode{Val: 5, Next: l21}
+	result := addTwoNumbers(l11, l20)
+	for result != nil {
+		fmt.Println(result.Val)
+		result = result.Next
+	}
 }
