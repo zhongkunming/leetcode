5db8b892a98ba44be2450ae56972c564a9f04586
zhongkunming <zhongkmi@163.com>
2022-04-17 10:43:02 +0800

    6.Z字形变换

---
+++ 6.Z字形变换/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := "PAYPALISHIRING"
+	rows := 4
+	fmt.Println(convert(str, rows))
+
+}
+
+func convert(s string, numRows int) string {
+
+	return ""
+}
